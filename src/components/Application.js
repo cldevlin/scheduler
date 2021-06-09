2a82702a314845f9988ef48f3eb5294c17f0055e
Claire Devlin <claire.devlin@hotmail.com>
2021-06-09 19:07:06 -0400

    add scheduler API and axois

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from "components/DayList.js";
+import Appointment from "components/Appointment";
 
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
+    interview: {
+      student: "suzy",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
   },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "bob",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg"
+      }
+    }
+  }
 ];
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      // console.log("response.data", response.data);
+      setDays([...response.data])
+    })
+  }, [])
 
   return (
     <main className="layout">
@@ -47,7 +103,23 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />      </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment, index) => {
+          console.log();
+          return index !== appointment.length - 1 ?
+            <Appointment
+              key={appointment.id}
+              {...appointment}
+            // time={appointment.time}
+            // interview={appointment.interview && {
+            //   student: appointment.interview.student,
+            //   interviewer: {
+            //     id: appointment.interview.interviewer.id,
+            //     name: appointment.interview.interviewer.name,
+            //     avatar: appointment.interview.interviewer.avatar
+            //   }
+            // }}
+            /> : <Appointment key="last" time="7pm" />
+        })}
       </section>
     </main>
   );
